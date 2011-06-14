20fe2ce556205a8c9feb4b4b950754333fc1810d
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-06-14 13:49:16 +0100

    RobotState is now in a separate file and a singleton

--- pr2_control_utilities/src/pr2_control_utilities/__init__.py
+++ pr2_control_utilities/src/pr2_control_utilities/__init__.py
@@ -1,8 +1,10 @@
+__all__ = ["PR2JointMover", "RobotState", "PR2BaseMover", "IKUtilities", "PR2MoveArm"]
+
 import roslib
 roslib.load_manifest("pr2_control_utilities")
 
 from pr2_joint_mover import PR2JointMover
-from pr2_joint_mover import RobotState
+from robot_state import RobotState
 from pr2_base_mover import PR2BaseMover
 from kinematics_loader import KinematicsLoader
 from interpolated_ik_motion_planner.ik_utilities import IKUtilities
--- pr2_control_utilities/src/pr2_control_utilities/pr2_joint_mover.py
+++ pr2_control_utilities/src/pr2_control_utilities/pr2_joint_mover.py
@@ -38,171 +38,17 @@ import exceptions
 from geometry_msgs.msg import Twist, Pose, PoseStamped, TwistStamped, PointStamped
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from pr2_controllers_msgs.msg import *
-from motion_planning_msgs.srv import (FilterJointTrajectoryWithConstraints,
-                                      FilterJointTrajectoryWithConstraintsRequest, 
-                                      FilterJointTrajectoryWithConstraintsResponse)
+from pr2_controllers_msgs.msg import (JointTrajectoryAction,
+                                      Pr2GripperCommandAction,
+                                      SingleJointPositionAction,
+                                      PointHeadAction)
+#from motion_planning_msgs.srv import (FilterJointTrajectoryWithConstraints,
+#                                      FilterJointTrajectoryWithConstraintsRequest, 
+#                                      FilterJointTrajectoryWithConstraintsResponse)
 
-from xml.dom import minidom
 
-class RobotState(object):
-    '''
-    A class to keep track of all the joint positions, divided by category.
-    
-    Use self.left_arm_positions right_arm_positions and self.head_positions to access the joint values.
-    '''
-    
-    left_joint_names = ('l_shoulder_pan_joint', 
-                           'l_shoulder_lift_joint',
-                           'l_upper_arm_roll_joint',
-                           'l_elbow_flex_joint',
-                           'l_forearm_roll_joint',
-                           'l_wrist_flex_joint',
-                           'l_wrist_roll_joint')
-    right_joint_names = ('r_shoulder_pan_joint', 
-                           'r_shoulder_lift_joint',
-                           'r_upper_arm_roll_joint',
-                           'r_elbow_flex_joint',
-                           'r_forearm_roll_joint',
-                           'r_wrist_flex_joint',
-                           'r_wrist_roll_joint')
-    
-    l_gripper_names = ('l_gripper_joint',)        
-    r_gripper_names = ('r_gripper_joint',)
-    
-    head_joint_names = ('head_pan_joint', 'head_tilt_joint')
-    
-    torso_joint_names = ("torso_lift_joint",)
-        
-    all = (left_joint_names + 
-           right_joint_names + 
-           head_joint_names + 
-           l_gripper_names +  
-           r_gripper_names +
-           torso_joint_names
-           )
-    
-    def __init__(self):
-        rospy.Subscriber("joint_states", JointState, self.joint_states_callback)
-        
-        self.left_arm_positions = []
-        self.right_arm_positions = []
-        self.head_positions = []
-        self.l_gripper_positions = []
-        self.r_gripper_positions = []
-        self.torso_position = []
-        self.joint_limits = {}
-        
-        self.r_arm_client = actionlib.SimpleActionClient("r_arm_controller/joint_trajectory_action", JointTrajectoryAction)        
-        self.l_arm_client = actionlib.SimpleActionClient("l_arm_controller/joint_trajectory_action", JointTrajectoryAction)
-        self.r_gripper_client = actionlib.SimpleActionClient("r_gripper_controller/gripper_action", Pr2GripperCommandAction)        
-        self.l_gripper_client = actionlib.SimpleActionClient("l_gripper_controller/gripper_action", Pr2GripperCommandAction)
-        self.torso_client = actionlib.SimpleActionClient("torso_controller/position_joint_action", SingleJointPositionAction)
-        self.head_pointer_client = actionlib.SimpleActionClient("head_traj_controller/point_head_action", PointHeadAction)
-        self.head_client = rospy.Publisher('/head_traj_controller/command', JointTrajectory, latch=True)
-        
-        # Some seemingly very important waits........
-        rospy.loginfo("Waiting for joint trajectory actions")
-        self.r_arm_client.wait_for_server()
-        self.l_arm_client.wait_for_server()
-        self.r_gripper_client.wait_for_server()
-        self.l_gripper_client.wait_for_server()
-        self.head_pointer_client.wait_for_server()
-        self.torso_client.wait_for_server()
-        
-        self.r_arm_client.cancel_all_goals()
-        self.l_arm_client.cancel_all_goals()
-        self.r_gripper_client.cancel_all_goals()
-        self.l_gripper_client.cancel_all_goals()
-        self.head_pointer_client.cancel_all_goals()
-        self.torso_client.cancel_all_goals()
-        
-        self.read_joint_limits()
-        
-        rospy.loginfo("Robot State is ready")
-    
-    def read_joint_limits(self):
-        urdf = rospy.get_param("robot_description")
-        try:
-            urdf_doc = minidom.parseString(urdf)
-        except IOError, e:
-            rospy.loginfo("robot_description failed to parse. Are you sure it valid?\nError is: %s" %e)
-            return
-        
-        joints = urdf_doc.getElementsByTagName('joint')
-        for jnt in joints:
-            name = jnt.attributes['name'].value
-            
-            lims = jnt.getElementsByTagName('limit')
-            if len(lims) == 0:
-                continue
-    
-            lower = lims[0].getAttribute('lower')
-            upper = lims[0].getAttribute('upper')
-    
-            # Ignore joints with no limits
-            if lower == '' or upper == '':
-                continue
-    
-            self.joint_limits[name] = (float(lower), float(upper))
-            
-    def __getstate__(self):
-        '''
-        Used for pickle.
-        '''
-        return "bogus"
-    
-    def __setstate__(self, state):        
-        '''
-        Used for pickle.
-        '''
-        #state is bogus
-        self.__init__()
 
-    
-    def joint_states_callback(self, msg):
-        self.left_arm_positions = []
-        for joint in self.left_joint_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.left_arm_positions.append(msg.position[index])
-            
-        self.right_arm_positions = []
-        for joint in self.right_joint_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.right_arm_positions.append(msg.position[index])
-            
-        self.head_positions = []
-        for joint in self.head_joint_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.head_positions.append(msg.position[index])
-        
-        self.l_gripper_positions = []
-        for joint in self.l_gripper_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.l_gripper_positions.append(msg.position[index])    
-        
-        self.r_gripper_positions = []
-        for joint in self.r_gripper_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.r_gripper_positions.append(msg.position[index])
-            
-        self.torso_position = []
-        for joint in self.torso_joint_names:
-            if not joint in msg.name:
-                rospy.logerr("Error: joint %s is not in the joint_states!"%joint)
-            index = msg.name.index(joint)
-            self.torso_position.append(msg.position[index])
-        
+from pr2_control_utilities.robot_state import RobotState
         
 class PR2JointMover(object):
     '''
@@ -976,16 +822,24 @@ def test_joint_limits():
     state.read_joint_limits()
     rospy.loginfo("Limits: %s" %str(state.joint_limits))
 
+def test_singleton():
+    
+    state1 = RobotState()
+    state2 = RobotState()
+    
+    rospy.loginfo("State1: %s, State2: %s"%(str(state1), str(state2)))
+    rospy.loginfo("Id1: %d, Id2:%d"%(id(state1),id(state2)))
+    rospy.loginfo("Equal? %s"%str(state1 == state2))
+
 if __name__ == "__main__":
     import os
     
     rospy.init_node('trytest', anonymous=True)
+    test_singleton()
 #    test_move_torso()
 #    test_open_file()
 
-    test_joint_limits()
+#    test_joint_limits()
     
     rospy.loginfo("Done")
-    while not rospy.is_shutdown():
-        rospy.sleep(0.1)    
     
