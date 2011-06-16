ccd63d51d4b6d6b965f2e0f0b72e149a375799d6
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-06-16 18:58:28 +0100

    The PR2BaseMover now works properly

--- pr2_control_utilities/src/pr2_control_utilities/pr2_base_mover.py
+++ pr2_control_utilities/src/pr2_control_utilities/pr2_base_mover.py
@@ -33,112 +33,156 @@ roslib.load_manifest("pr2_control_utilities")
 
 import rospy
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped, PointStamped
+
 import tf
 import math
 import pr2_control_utilities
 
 def sign(x):
-    if x>0:
+    if x>=0:
         return 1.
     else:
         return -1.
 
+def sign_zero(x):
+    if x > 0:
+        return 1.
+    elif x < 0:
+        return -1.
+    else:
+        return 0
+
 class PR2BaseMover(object):
     def __init__(self):        
         self.listener = tf.TransformListener()
+        print tf.listener
         
         self.cmd_vel_pub = rospy.Publisher("/base_controller/command", Twist)
         
         self.max_vel = 0.25
-        self.min_vel = 0.1
-        self.min_rot = 0.1
-        self.max_rot = 0.1
+        self.min_vel = 0.0
+        self.min_rot = 0.0
+        self.max_rot = math.pi/4
        
-    def drive_to_displacement(self, pos, inhibit_x = False, inhibit_y = False, inhibit_theta = False):
+    def drive_to_displacement(self, pos,
+                              inhibit_x = False,
+                              inhibit_y = False,
+                              inhibit_theta = False):
         '''
         Drive to the relative pos (diplacement)
         @param pos: tuple: x,y,theta        
         '''
         
-        self.listener.waitForTransform("base_footprint", "odom_combined",
-                                        rospy.Time(0), rospy.Duration(1))        
+        desired_pos = self.convert_point((pos[0],pos[1],0),
+                                            "/base_link", 
+                                            "odom_combined")
+        _ , rot = self.current_position()        
+        desired_rot = rot[2] + pos[2]
         
-        (start_trans,start_rot) = self.listener.lookupTransform("base_footprint", 
-                                                                "odom_combined",
-                                                                rospy.Time(0))
-        
-        desired_trans = (start_trans[0] + pos[0],
-                         start_trans[1] + pos[1],
-                         start_trans[2])
-        desired_rot = (start_rot[0],
-                       start_rot[1],
-                       start_rot[2] + pos[2])
         rate = rospy.Rate(10)
-        
         while not rospy.is_shutdown():
-            (curr_trans, curr_rot) = self.listener.lookupTransform("base_footprint", 
-                                                                   "odom_combined",
-                                                                   rospy.Time(0))
+            (curr_trans, curr_rot) = self.current_position()
             if not inhibit_x:
-                dx = curr_trans[0] - desired_trans[0]
+                dx = desired_pos[0] - curr_trans[0]
             else:
                 dx = 0
             if not inhibit_y:
-                dy = curr_trans[1] - desired_trans[1]
+                dy = desired_pos[1] - curr_trans[1]
             else:
                 dy = 0
             if not inhibit_theta:
-                dtheta = curr_rot[2] - desired_rot[2]
+                dtheta = desired_rot - curr_rot[2]
             else:
                 dtheta = 0
+
+            #the velocity vector has to be rotated in the base_frame reference            
+            vx = dx * math.cos(-curr_rot[2]) - dy * math.sin(-curr_rot[2])
+            vy = dx * math.sin(-curr_rot[2]) + dy * math.cos(-curr_rot[2])
             
-#            rospy.loginfo("DX: %s"%str((dx,dy,dtheta)))
-            
-            if (math.fabs(dx) < 0.01 and
-                math.fabs(dy) < 0.01 and
-                math.fabs(dtheta) < 0.01):                
+            if (math.fabs(vx) < 0.05 and
+                math.fabs(vy) < 0.05 and
+                math.fabs(dtheta) < 0.05):
                 break
             
             base_cmd = Twist()
             
-            if math.fabs(dx) < 0.01:
-                base_cmd.linear.x = 0
+            if math.fabs(vx) < self.min_vel:
+                base_cmd.linear.x = sign(vx) * self.min_vel
+            elif math.fabs(vx) > self.max_vel:
+                base_cmd.linear.x = sign(vx) * self.max_vel
             else:
-                if math.fabs(dx) < self.min_vel:
-                    base_cmd.linear.x = sign(dx) * self.min_vel
-                elif math.fabs(dx) > self.max_vel:
-                    base_cmd.linear.x = sign(dx) * self.max_vel
-                else:
-                    base_cmd.linear.x = dx
-            
-            if math.fabs(dy) < 0.01:
-                base_cmd.linear.y = 0
+                base_cmd.linear.x = vx
+        
+            if math.fabs(vy) < self.min_vel:
+                base_cmd.linear.y = sign(vy) * self.min_vel
+            elif math.fabs(vy) > self.max_vel:
+                base_cmd.linear.y = sign(vy) * self.max_vel
             else:
-                if math.fabs(dy) < self.min_vel:
-                    base_cmd.linear.y = sign(dy) * self.min_vel
-                elif math.fabs(dy) > self.max_vel:
-                    base_cmd.linear.y = sign(dy) * self.max_vel
-                else:
-                    base_cmd.linear.y = dy
-            
-            if math.fabs(dtheta) < 0.01:
-                base_cmd.angular.z = 0
+                base_cmd.linear.y = vy
+        
+            if math.fabs(dtheta) < self.min_rot:
+                base_cmd.angular.z = sign(dtheta) * self.min_rot
+            elif math.fabs(dtheta) > self.max_rot:
+                base_cmd.angular.z = sign(dtheta) * self.max_rot
             else:
-                if math.fabs(dtheta) < self.min_rot:
-                    base_cmd.angular.z = sign(dtheta) * self.min_rot
-                elif math.fabs(dx) > self.max_rot:
-                    base_cmd.angular.z = sign(dtheta) * self.max_rot
-                else:
-                    base_cmd.angular.z = dtheta
+                base_cmd.angular.z = dtheta
             
-#            rospy.loginfo("CMD: %s" % str(base_cmd))
             self.cmd_vel_pub.publish(base_cmd)
             rate.sleep()
                             
+    def current_position(self, frame="/odom_combined"):
+        
+        return self.convert_position((0,0,0), 
+                                       (0,0,0), 
+                                       "/base_link", 
+                                       frame)
+        
+    def convert_position(self, pos, rot, from_frame, to_frame):
+        self.listener.waitForTransform(to_frame, from_frame,
+                                        rospy.Time(0), rospy.Duration(1))
+
+        zeropose = PoseStamped()
+        zeropose.header.frame_id = from_frame
+        zeropose.pose.position.x = pos[0]
+        zeropose.pose.position.y = pos[1]
+        zeropose.pose.position.z = pos[2]
+        quaternion = tf.transformations.quaternion_from_euler(rot[0], rot[1], rot[2], axes="rxyz")
+        zeropose.pose.orientation.x = quaternion[0]
+        zeropose.pose.orientation.y = quaternion[1]
+        zeropose.pose.orientation.z = quaternion[2]
+        zeropose.pose.orientation.w = quaternion[3]
+        
+#        zeropose.header.stamp = rospy.Time.now()
+        newpose = self.listener.transformPose(to_frame, zeropose)
+        trans = (newpose.pose.position.x,
+                 newpose.pose.position.y,
+                 newpose.pose.position.z)
+        quaternion = (newpose.pose.orientation.x,
+                      newpose.pose.orientation.y,
+                      newpose.pose.orientation.z,
+                      newpose.pose.orientation.w)
+        rot = tf.transformations.euler_from_quaternion(quaternion,axes="rxyz")
+        return trans,rot
+        
+    def convert_point(self, pos, from_frame, to_frame):
+        self.listener.waitForTransform(to_frame, from_frame,
+                                        rospy.Time(0), rospy.Duration(1))
+
+        zeropose = PointStamped()
+        zeropose.header.frame_id = from_frame
+        zeropose.point.x = pos[0]
+        zeropose.point.y = pos[1]
+        zeropose.point.z = pos[2]
+        
+        newpose = self.listener.transformPoint(to_frame, zeropose)
+        return newpose.point.x, newpose.point.y, newpose.point.z
+        
+        
 
 def test_move():
     mover = PR2BaseMover()
-    mover.drive_to_displacement((0,-0.0,0.1))
+    mover.drive_to_displacement((0.0, +0.0, -math.pi/2))
     
 if __name__ == "__main__":
     rospy.init_node("try_move", anonymous=True)
