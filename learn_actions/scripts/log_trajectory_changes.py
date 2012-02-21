710319662b7c801cf15b45bf5aff42df88964bfb
Lorenzo Riano <lorenzo.riano@gmail.com>
2012-02-21 17:44:24 +0000

    more generic

--- learn_actions/scripts/log_trajectory_changes.py
+++ learn_actions/scripts/log_trajectory_changes.py
@@ -8,7 +8,6 @@ roslib.load_manifest('learn_actions')
 import rospy
 import pr2_control_utilities
 import tabletop_actions.object_detector as object_detector
-import tabletop_actions.pushers as pushers
 
 import learn_actions 
 
@@ -17,11 +16,14 @@ def main():
     joint_mover = pr2_control_utilities.PR2JointMover(robot_state)
     planner = pr2_control_utilities.PR2MoveArm(joint_mover)
     detector = object_detector.ObjectDetector()
-
+    
+    arm = "left"
+    #arm = "right"
+    service_name = "/pr2_trajectory_markers_" + arm + "/execute_trajectory"
     logger = learn_actions.LogTrajectoryResult(detector, 
                                             joint_mover,
-                                            "/pr2_trajectory_markers_right/execute_trajectory",
-                                            "right",
+                                            service_name,
+                                            arm,
                                             planner,
                                             tf_listener = planner.tf_listener)
     raw_input("press a key to start...")
